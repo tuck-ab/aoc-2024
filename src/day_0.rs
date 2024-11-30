ff4200afaed643909c6829625d9444741bf8a52c
Xander Tuck <tuckabh@gmail.com>
2024-11-30 16:21:02 +0000

    arg parsing

---
+++ src/day_0.rs
@@ -0,0 +1,13 @@
+use crate::Solution;
+
+pub(crate) struct Day0;
+
+impl Solution for Day0 {
+    fn part_1() {
+        println!("Day 0, part 1")
+    }
+
+    fn part_2() {
+        println!("Day 0, part 2")
+    }
+}
---
+++ src/main.rs
@@ -0,0 +1,38 @@
+mod day_0;
+
+use clap::Parser;
+
+/// Args doc comment
+#[derive(Parser, Debug)]
+#[command(version, about, long_about = None)]
+struct Args {
+    /// Puzzle day
+    #[arg(short, long)]
+    day: u8,
+
+    /// Puzzle part
+    #[arg(short, long)]
+    part: u8,
+}
+
+pub(crate) trait Solution {
+    fn part_1();
+    fn part_2();
+}
+
+fn main() {
+    let args = Args::parse();
+
+    match args.day {
+        0 => run::<day_0::Day0>(args.part),
+        _ => panic!("Day '{}' not implemented", args.day),
+    }
+}
+
+fn run<T: Solution>(part: u8) {
+    match part {
+        1 => T::part_1(),
+        2 => T::part_2(),
+        _ => panic!("Unexpected part '{}'", part),
+    }
+}
