b7482f57c4b31b7a455ed445328bb6f396b4b33f
Xander Tuck <tuckabh@gmail.com>
2024-12-07 10:59:34 +0000

    day 7 refactor

--- src/day_7.rs
+++ src/day_7.rs
@@ -5,39 +5,19 @@ use crate::Solution;
 
 pub(crate) struct Day7;
 
-fn get_op(op: usize) -> fn((i128, i128)) -> i128 {
-    match op {
-        0 => |(a, b)| a + b,
-        1 => |(a, b)| a * b,
-        2 => |(a, b)| a * 10_i128.pow((b as f64 + 0.1).log10().ceil() as u32) + b,
-        _ => panic!("Op {} not found", op)
-    }
-}
-
 impl Solution for Day7 {
     fn part_1() -> String {
         let data = load_input(7);
 
         let mut total = 0;
-        'outer: for (t, s) in data.lines().map(|l| l.split(": ").collect_tuple().unwrap()) {
+        for (t, s) in data.lines().map(|l| l.split(": ").collect_tuple().unwrap()) {
             let target: i128 = t.parse().expect("Couldn't parse number");
             let vals: Vec<i128> = s
                 .split(" ")
                 .map(|l| l.parse().expect("Couldn't parse nubmer"))
                 .collect();
-
-            for ops in repeat_n(0_usize..2_usize, vals.len() - 1).multi_cartesian_product() {
-                let mut calc = vals[0];
-                for (val, op) in vals[1..].iter().zip(ops.iter()) {
-                    calc = get_op(*op)((calc, *val));
-                }
-
-                if calc == target {
-                    total += calc;
-                    continue 'outer;
-                }
-            }
-        }
+            total += run_line(target, &vals, &vec![Op::Add, Op::Mul]).unwrap_or(0);
+        } 
 
         total.to_string()
     }
@@ -46,29 +26,45 @@ impl Solution for Day7 {
         let data = load_input(7);
 
         let mut total = 0;
-        'outer: for (t, s) in data.lines().map(|l| l.split(": ").collect_tuple().unwrap()) {
+        for (t, s) in data.lines().map(|l| l.split(": ").collect_tuple().unwrap()) {
             let target: i128 = t.parse().expect("Couldn't parse number");
             let vals: Vec<i128> = s
                 .split(" ")
                 .map(|l| l.parse().expect("Couldn't parse nubmer"))
                 .collect();
-
-            'perm: for ops in repeat_n(0_usize..3_usize, vals.len() - 1).multi_cartesian_product() {
-                let mut calc = vals[0];
-                for (val, op) in vals[1..].iter().zip(ops.iter()) {
-                    calc = get_op(*op)((calc, *val));
-                    if calc > target {
-                        continue 'perm;
-                    }
-                }
-
-                if calc == target {
-                    total += calc;
-                    continue 'outer;
-                }
-            }
+            total += run_line(target, &vals, &vec![Op::Add, Op::Mul, Op::Cat]).unwrap_or(0);
         }
 
         total.to_string()
     }
+}
+
+enum Op {
+    Add,
+    Mul,
+    Cat,
+}
+
+impl Op {
+    fn calc(&self, a: i128, b: i128) -> i128 {
+        match *self {
+            Op::Add => a + b,
+            Op::Mul => a * b,
+            Op::Cat => a * 10_i128.pow((b as f64 + 0.1).log10().ceil() as u32) + b,
+        }
+    }
+}
+
+fn run_line(target: i128, line: &Vec<i128>, ops: &Vec<Op>) -> Option<i128> {
+    for ops in repeat_n(ops, line.len() - 1).multi_cartesian_product() {
+        let mut calc = line[0];
+        for (val, op) in line[1..].iter().zip(ops.iter()) {
+            calc = op.calc(calc, *val);
+        }
+
+        if calc == target {
+            return Some(target);
+        }
+    }
+    None
 }
