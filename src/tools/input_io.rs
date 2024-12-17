dc28949383ceb0c69c780b1626b0aaf15b090eb1
Xander Tuck <tuckabh@gmail.com>
2024-12-17 22:30:00 +0000

    adding debug env var support

--- src/tools/input_io.rs
+++ src/tools/input_io.rs
@@ -1,10 +1,11 @@
+use std::env;
 use std::fs;
 
 pub fn load_input(day: u8) -> String {
-    fs::read_to_string(format!("inputs/day{}.txt", day)).expect("Could not open input file")
-}
-
-#[allow(dead_code)]
-pub fn load_demo(day: u8) -> String {
-    fs::read_to_string(format!("demos/day{}.txt", day)).expect("Could not open input file")
+    let debug = env::var("DEBUG").unwrap_or("false".to_string());
+    if debug == "true" {
+        fs::read_to_string(format!("demos/day{}.txt", day)).expect("Could not open demo file")
+    } else {
+        fs::read_to_string(format!("inputs/day{}.txt", day)).expect("Could not open input file")
+    }
 }
--- src/tools/mod.rs
+++ src/tools/mod.rs
@@ -2,5 +2,5 @@ pub mod input_io;
 pub mod vec2d;
 
 #[allow(unused_imports)]
-pub use input_io::{load_demo, load_input};
+pub use input_io::load_input;
 pub use vec2d::{Coord, Dir4, Vec2D, DIR4S};
